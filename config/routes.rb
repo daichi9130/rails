83017fd894f8e8ef7670e907ac95562ed49263f9
EC2 Default User <ec2-user@ip-172-31-37-248.ec2.internal>
2021-10-12 12:47:45 +0000

    [finish]2-56

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ get 'todolists/new'
 get 'top' => 'homes#top'
 post 'todolists' => 'todolists#create'
 get 'todolists' => 'todolists#index'
-
-
+get 'todolists/:id' => 'todolists#show',as:'todolist'
+get 'todolists/:id/edit' => 'todolists#edit',as:'edit_todolist'
+patch 'todolists/:id' => 'todolists#update',as: 'update_todolist'
 end
 
 
